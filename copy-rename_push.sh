bb0f81e10cff4e9015bef2e74371ca7f4a3b1f1f
dracomalar <dracomalar@gmail.com>
2023-02-09 16:45:09 -0800

    update

--- copy-rename_push.sh
+++ copy-rename_push.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Takes two program arguments - copies <oldFolderName> and renames to argument <newFolderName>
-oldFolderName=$1
-newFolderName=$2
+repoName=$1
+oldFolderName=$2
+newFolderName=$3
+
 
 # Copy projectStarter and rename it to newFolderName
-cp -r ~/Desktop/Git-Hub/50-Days-50-Projects/$oldFolderName ~/Desktop/Git-Hub/50-Days-50-Projects/$newFolderName
+cp -r ~/Desktop/Git-Hub/$repoName/$oldFolderName ~/Desktop/Git-Hub/$repoName/$newFolderName
 
 # Add the copied folder to Git
-git add ~/Desktop/Git-Hub/50-Days-50-Projects/$newFolderName
+git add ~/Desktop/Git-Hub/$repoName/$newFolderName
 
 # Commit the added folder to Git with a message
 git commit -m "New Project"
--- project-template.sh
+++ project-template.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Take one program arguments - copies <_project_starter_> and renames to argument <newFolderName>
-newFolderName=$1
+repoName=$1
+newFolderName=$2
 
 # Copy projectStarter and rename it to newFolderName
-cp -r ~/Desktop/Git-Hub/50-Days-50-Projects/_project_starter_ ~/Desktop/Git-Hub/50-Days-50-Projects/$newFolderName
+cp -r ~/Desktop/Git-Hub/$repoName/_project_starter_ ~/Desktop/Git-Hub/$repoName/$newFolderName
 
 # Add the copied folder to Git
-git add ~/Desktop/Git-Hub/50-Days-50-Projects/$newFolderName
+git add ~/Desktop/Git-Hub/$repoName/$newFolderName
 
 # Commit the added folder to Git with a message
 git commit -m "New Project"
